427e0a923b35f43ef8c2b8511e348f6c6d13d602
Stephen Brough <StephenR.Brough@gmail.com>
2013-11-10 15:39:44 -0700

    Added keyup functionality to controller

--- js/movieCtrl.js
+++ js/movieCtrl.js
@@ -4,10 +4,20 @@
  *  Controller for the Aggregate Movie App
  *
  *  author: Stephen Brough
+ *  version: 0.1
+ *  Date: November 10, 2013
  */
  
  var app = angular.module('movieApp', []);
  
  app.controller('movieCtrl', function($scope){
 		$scope.message = "Test";
+		
+		// Keyup callback for the search input
+		$scope.keyUp = function(keyEvent) {
+			$scope.message = $scope.movieSearch;
+			console.log('keyup', keyEvent); // FOR TESTING
+		};
  });
+ 
+ 
