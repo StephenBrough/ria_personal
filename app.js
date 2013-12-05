9521e7d0baf00a4210fe0e699de2d97bd55e6a17
Stephen Brough <StephenR.Brough@blotcoo.com>
2013-12-04 23:26:53 -0700

    Added request library; Node can call rotten tomatoes api

---
+++ app.js
@@ -0,0 +1,57 @@
+/*
+ *  app.js
+ *  
+ *  NodeJS app file
+ *
+ *  author: Stephen Brough
+ *  version: 0.1
+ *  Date: December 3, 2013
+ */
+ 
+ var express = require('express');
+ var request = require('request');  
+ //var Arg = require('arg');
+ var app = express(); 
+ 
+ /* Rotten Tomatoes key variables */
+ var rottenKey = '74pt3qns49x8qu5a2fumx2qf';
+ var path = "http://api.rottentomatoes.com";
+ var endpoint = "/api/public/v1.0/movies.json"; 
+ /* * * * * * * * * * * * * * * * */
+ 
+ app.use('/js', express.static(__dirname + '/js')); 
+ 
+ app.get('/rottenApi/:page_limit/:page/:q', function(req, response) {
+   console.log(req.params);   
+   
+   var url = path + endpoint + '?' +
+   'q=' + req.params.q.split('%20').join('+') + '&'+
+   'page_limit=' + req.params.page_limit + '&' +
+   'page=' + req.params.page + '&' +
+   'apikey=' + rottenKey;
+   console.log('Path: ' + url);
+   
+   //var r = 
+   request(url, function (error, response, body) {
+     if (!error && response.statusCode == 200) {
+       console.log(body); // Print the google web page.
+     }        
+   }).pipe(response);  
+   //response.send(r);
+ });
+ 
+ /*app.get('/rottenApi', function(request, response) {
+  var r = req("http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=Butch+Cassidy&page_limit=10&page=1&apikey=74pt3qns49x8qu5a2fumx2qf", function (error, response, body) {
+     if (!error && response.statusCode == 200) {
+       console.log(body); // Print the google web page.
+     }        
+   });  
+   response.send(r);   
+ }); */
+ 
+ app.get('/', function(request, response) {
+    response.sendfile('index.html');
+ });
+
+ 
+ app.listen(3000); 
--- js/services.js
+++ js/services.js
@@ -7,34 +7,30 @@
  *  version: 0.1
  *  Date: December 3, 2013
  */
- 
- /* Rotten Tomatoes key variables */
- var rottenKey = '74pt3qns49x8qu5a2fumx2qf';
- var path = "http://api.rottentomatoes.com";
- var endpoint = "/api/public/v1.0/movies.json"; 
- /* * * * * * * * * * * * * * * * */
- 
+  
   var params = {    
     page_limit:10,   // The amount of movie search results to show per page
     page:1,          // The selected page of movie search results  
-    apikey:rottenKey // My personalized, fancy shmansy API key for Rotten Tomatoes!
+    //apikey:rottenKey // My personalized, fancy shmansy API key for Rotten Tomatoes!
   };
+  
+  var rottenPath = '/rottenApi';
  
  var services = angular.module('services', []);
  
  services.factory('Movies', ['$http', function($http) {
    return {
-     get: function(args, callback) {
-        params.q = args;
+     get: function(args, callback) {        
         $http({
           method: 'GET', 
-          url:Arg.url(path + endpoint, params)
+          url: rottenPath + '/' + params.page_limit + '/' + params.page + '/' + args
           //headers: {'Access-Control-Allow-Origin' : '*'}
           })
           .success(function(data, status, headers, config){
+            console.log(data);
             callback(data);
           });
-        console.log("Rotten Tomatoes URL: " + Arg.url(path + endpoint, params));
+        console.log("Rotten Tomatoes URL: " + rottenPath + '/' + params.page_limit + '/' + params.page + '/' + args);
      }
    };
  }]);
